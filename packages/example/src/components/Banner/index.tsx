560994ef01dc3bd814c6eaf7adf8315344f09ccd
aaronisme <aarondongchen@gmail.com>
2022-04-15 12:58:51 +0800

    add info box to install metamask flask first

--- packages/example/src/components/Banner/index.tsx
+++ packages/example/src/components/Banner/index.tsx
@@ -1,14 +1,27 @@
-import React from "react"
-import banner from './banner.png'
-import { Container } from "semantic-ui-react"
-
+import React from 'react';
+import banner from './banner.png';
+import { Container, Label } from 'semantic-ui-react';
 
 const Banner = () => {
-    return (
-        <Container className="Banner-container">
-            <img src={banner} alt="" className="Img-box"/>
-        </Container>
-    )
-}
+  return (
+    <Container className="Banner-container" vertical="center">
+      <img src={banner} alt="" className="Img-box" />
+      <p className="App-Container">
+        <Label color="blue">
+          {' '}
+          Please ensure you have Metamask Flask
+          Installed for this example,{' '}
+          <a
+            href="https://chrome.google.com/webstore/detail/metamask-flask-developmen/ljfoeinjpaedjfecbmggjgodbgkmjkjk"
+            className="Banner-link"
+            target="_blank">
+            {' '}
+            Click here to install
+          </a>
+        </Label>
+      </p>
+    </Container>
+  );
+};
 
-export default Banner
+export default Banner;
