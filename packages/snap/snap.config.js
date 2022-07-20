c326ccea23b26885a388d144d3a9480cc856fd40
aaronisme <aarondongchen@gmail.com>
2022-07-20 16:43:49 +0800

    update code for Metamask Flask 10.17 API Change

---
+++ packages/snap/snap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  cliOptions: {
+    "dist": "dist",
+    "outfileName": "bundle.js",
+    "port":8081,
+    "src": "dist/index.js"
+  },
+};
--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,1 +1,22 @@
-import "./snap"
+import { getNetwork } from './bitcoin';
+import {Wallet, ScriptType, MetamaskBTCRpcRequest} from './interface'
+import { getExtendedPublicKey, signPsbt} from './rpc'
+
+declare let wallet: Wallet;
+  
+type rpcReqeust = {
+  origin: string
+  request: MetamaskBTCRpcRequest
+}
+
+export const onRpcRequest = async({origin, request}:rpcReqeust) => {
+  switch (request.method) {
+    case 'btc_getPublicExtendedKey':
+      return getExtendedPublicKey(wallet, ScriptType.P2PKH, getNetwork(request.params.network))
+    case 'btc_signPsbt':
+      const psbt = request.params.psbt;
+      return signPsbt(wallet, psbt, getNetwork(request.params.network))
+    default:
+      throw new Error('Method not found.');
+  }
+};
