4c22abfd32c34f9713b96fd45b2c8e308bf20944
yan.li <liyan1924@gmail.com>
2022-08-11 16:25:02 +0800

    Add Menu component

--- packages/example/src/components/Account/Aside.tsx
+++ packages/example/src/components/Account/Aside.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import Menu from "./image/menu.svg";
 import Refresh from "./image/refresh.svg";
 import Transactions from "./image/transactions.svg";
+import Menu from "../Menu";
 
 const Aside = () => {
   return (
     <div className="Account-Aside">
       <div className="Account-Aside-Container">
-        <div className="Menu-button">
-          <img src={Menu} alt="Menu" />
-        </div>
+        <Menu />
         <div className="Tx-list-container">
           <img src={Transactions} alt="transactions"/>
           <p className="Empty-tip">No Transactions</p>
