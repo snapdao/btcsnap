c885d91553ab56bd2a7aba0513724fca6fb60b80
soralit <soralitria@gmail.com>
2022-08-16 16:44:40 +0800

    fix(send): should reset state when open send modal

--- packages/example/src/components/Account/Main.tsx
+++ packages/example/src/components/Account/Main.tsx
@@ -1,29 +1,27 @@
 import React, { useCallback, useState } from 'react';
 import {observer} from 'mobx-react-lite';
+import { Modal } from "semantic-ui-react";
+
+import { useKeystoneStore } from "../../mobx";
+import {BitcoinNetwork, Utxo} from "../../interface";
+import SendModal from '../SendModal';
+
 import Logo from "./image/logo.svg";
 import LogoTestnet from "./image/logo-testnet.svg";
 import Send from "./image/send.svg";
-import { Modal } from "semantic-ui-react";
-import { useKeystoneStore } from "../../mobx";
-import { BitcoinNetwork } from "../../interface";
+import {SendInfo} from "../../lib";
+
 
 export interface MainProps {
   balance: number
   receiveAddress: string
+  utxos: Utxo[],
+  sendInfo?: SendInfo,
 }
 
-const Main = observer(({balance, receiveAddress}: MainProps) => {
+const Main = observer(({balance, receiveAddress, utxos, sendInfo}: MainProps) => {
   const { global: { network }} = useKeystoneStore();
-  const [showSendModal, setShowSendModal] = useState<boolean>(false)
   const [showReceiveModal, setShowReceiveModal] = useState<boolean>(false)
-
-  const onSend = useCallback(() => {
-    setShowSendModal(true);
-  }, [setShowSendModal])
-
-  const closeSendModal = useCallback(() => {
-    setShowSendModal(false);
-  }, [setShowSendModal])
 
   const onReceive = useCallback(() => {
     setShowReceiveModal(true)
@@ -34,62 +32,44 @@ const Main = observer(({balance, receiveAddress}: MainProps) => {
   }, [setShowReceiveModal]);
 
   return (
-    <div className="Account-Main">
-      <div className="Account-Main-Container">
-        <div className="Logo-container">
-          <img src={network === BitcoinNetwork.Main ? Logo : LogoTestnet} alt="BitcoinSnap" className="Logo-img" />
-          <span className="Logo-label">Alpha</span>
-        </div>
-        <div className="Balance-container">
-          <p className="Balance-label">current balance</p>
-          <p className="Balance">{balance} BTC</p>
-        </div>
-        <div>
-          <div className="Action-container" onClick={onSend}>
-            <div className="Action-button-container">
-              <img src={Send} alt="Send" />
-            </div>
-            <p>send</p>
+      <div className="Account-Main">
+        <div className="Account-Main-Container">
+          <div className="Logo-container">
+            <img src={network === BitcoinNetwork.Main ? Logo : LogoTestnet} alt="BitcoinSnap" className="Logo-img" />
+            <span className="Logo-label">Alpha</span>
           </div>
-          <div className="Action-container" onClick={onReceive}>
-            <div className="Action-button-container rotate">
-              <img src={Send} alt="Receive" />
+          <div className="Balance-container">
+            <p className="Balance-label">current balance</p>
+            <p className="Balance">{balance} BTC</p>
+          </div>
+          <div>
+            <div className="Action-container">
+              <SendModal network={network} utxos={utxos} sendInfo={sendInfo} />
+              <p>send</p>
             </div>
-            <p>receive</p>
+            <div className="Action-container" onClick={onReceive}>
+              <div className="Action-button-container rotate">
+                <img src={Send} alt="Receive" />
+              </div>
+              <p>receive</p>
+            </div>
           </div>
         </div>
-      </div>
-
-      {/*
-        TODO change with SendModal
-      */}
-      <Modal
-        onClose={closeSendModal}
-        open={showSendModal}
-      >
-        <Modal.Header>
-          Send
-        </Modal.Header>
-        <Modal.Content>
-          WIP: Send to
-        </Modal.Content>
-      </Modal>
-
-      {/*
+        {/*
         TODO change with ReceiveModal
       */}
-      <Modal
-        onClose={closeReceiveModal}
-        open={showReceiveModal}
-      >
-        <Modal.Header>
-          Receive
-        </Modal.Header>
-        <Modal.Content>
-          WIP: address: {receiveAddress} to QR Code
-        </Modal.Content>
-      </Modal>
-    </div>
+        <Modal
+            onClose={closeReceiveModal}
+            open={showReceiveModal}
+        >
+          <Modal.Header>
+            Receive
+          </Modal.Header>
+          <Modal.Content>
+            WIP: address: {receiveAddress} to QR Code
+          </Modal.Content>
+        </Modal>
+      </div>
   );
 });
 
