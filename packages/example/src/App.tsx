9d83d5f79dd3a322191a0b559c7713b45f1b90aa
yan.li <liyan1924@gmail.com>
2022-08-16 11:22:59 +0800

    Add font Opensans

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Account from "./components/Account";
 import Connect from "./components/Connect";
 import { getKeystoneStore, MobxStoreProvider } from "./mobx";
-import './App.css';
 
 function App() {
   const mobxStore = getKeystoneStore();
