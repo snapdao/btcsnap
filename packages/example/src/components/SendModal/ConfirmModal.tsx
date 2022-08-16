0d78f3c4d489ab03e8f66c9e10ad4d61c9954ba3
soralit <soralitria@gmail.com>
2022-08-16 17:17:12 +0800

    feat(send): refine ui

--- packages/example/src/components/SendModal/ConfirmModal.tsx
+++ packages/example/src/components/SendModal/ConfirmModal.tsx
@@ -1,8 +1,8 @@
 import SendViewModel from './model';
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import React, { FunctionComponent } from 'react';
 import { observer } from 'mobx-react-lite';
-import { Container, Divider, Modal } from 'semantic-ui-react';
-import close from '../../assets/close.png';
+import {Container, Divider, Loader, Modal} from 'semantic-ui-react';
+import close from '../../assets/close.svg';
 import BTCValue from './BTCValue';
 
 export type ConfirmModalProps = {
@@ -26,76 +26,81 @@ const ConfirmModal: FunctionComponent<ConfirmModalProps> = observer(props => {
           Send
         </button>
       }>
-      <Container className={'modal-content-container colored-container'}>
-        <div className={'modal-header'}>
-          <span />
-          <span className={'text-weight-bold'}>Confirm Transaction</span>
-          <img
-            style={{ cursor: 'pointer' }}
-            src={close}
-            width={12}
-            alt={'close'}
-            onClick={() => model.setConfirmOpen(false)}
-          />
-        </div>
-        <div className={'modal-section vertical-center'}>
+      {model.isSending ? <>
+        <Loader />
+      </>: <>
+        <Container className={'modal-content-container colored-container'}>
+          <div className={'modal-header'}>
+            <span />
+            <span className={'text-weight-bold'}>Confirm Transaction</span>
+            <img
+                style={{ cursor: 'pointer' }}
+                src={close}
+                width={12}
+                alt={'close'}
+                onClick={() => model.setConfirmOpen(false)}
+            />
+          </div>
+          <div className={'modal-section vertical-center'}>
           <span className={'text-weight-bold text-secondary'}>
             You're Sending
           </span>
-          <span style={{ marginTop: 8 }}>
+            <span style={{ marginTop: 8 }}>
             <BTCValue
-              value={model.amountText}
-              size={'large'}
-              fontWeight={'normal'}
+                value={model.amountText}
+                size={'large'}
+                fontWeight={'normal'}
             />
           </span>
-          <span
-            className={'text-weight-bold text-secondary'}
-            style={{ marginTop: 12 }}>
+            <span
+                className={'text-weight-bold text-secondary'}
+                style={{ marginTop: 12 }}>
             To
           </span>
-          <span style={{ marginTop: 12 }}>{model.to}</span>
-        </div>
-      </Container>
-      <Container className={'modal-content-container'}>
-        <div className={'modal-section'}>
-          <div className={'flex row space-between'}>
-            <span className={'text-secondary text-weight-bold'}>Amount</span>
-            <BTCValue
-              value={model.amountText}
-              size={'normal'}
-              fontWeight={'normal'}
-            />
+            <span style={{ marginTop: 12 }}>{model.to}</span>
           </div>
-          <div className={'flex row space-between'} style={{ marginTop: 20 }}>
-            <span className={'text-secondary text-weight-bold'}>Fee</span>
-            <BTCValue
-              value={model.feeText}
-              size={'normal'}
-              fontWeight={'normal'}
-            />
-          </div>
-          <Divider />
-          <div className={'flex row space-between'}>
+        </Container>
+        <Container className={'modal-content-container'}>
+          <div className={'modal-section'}>
+            <div className={'flex row space-between'}>
+              <span className={'text-secondary text-weight-bold'}>Amount</span>
+              <BTCValue
+                  value={model.amountText}
+                  size={'normal'}
+                  fontWeight={'normal'}
+              />
+            </div>
+            <div className={'flex row space-between'} style={{ marginTop: 20 }}>
+              <span className={'text-secondary text-weight-bold'}>Fee</span>
+              <BTCValue
+                  value={model.feeText}
+                  size={'normal'}
+                  fontWeight={'normal'}
+              />
+            </div>
+            <Divider />
+            <div className={'flex row space-between'}>
             <span className={'text-secondary text-weight-bold'}>
               Total Amount
             </span>
-            <BTCValue
-              value={model.totalAmount}
-              size={'normal'}
-              fontWeight={'bold'}
-            />
+              <BTCValue
+                  value={model.totalAmount}
+                  size={'normal'}
+                  fontWeight={'bold'}
+              />
+            </div>
           </div>
-        </div>
-        <button
-          style={{ marginTop: 72 }}
-          className={
-            'action-button action-button-primary action-button-size-full-width'
-          }
-          onClick={model.send}>
-          Confirm
-        </button>
-      </Container>
+          <button
+              style={{ marginTop: 72 }}
+              className={
+                'action-button action-button-primary action-button-size-full-width'
+              }
+              onClick={model.send}>
+            Confirm
+          </button>
+        </Container>
+      </>}
+
     </Modal>
   );
 });
