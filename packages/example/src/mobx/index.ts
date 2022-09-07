40a16a04a5e8dfdac68f59a323b5a421e576e2dc
LiYanLance <liyan1924@gmail.com>
2022-09-07 13:37:40 +0800

    Loading when register and fetch balance

--- packages/example/src/mobx/index.ts
+++ packages/example/src/mobx/index.ts
@@ -1,18 +1,18 @@
 import { createContext, useContext } from "react";
 import KeystoneStore, {storeInitialState} from './store';
+import { IStore } from "./types";
 export * as utils from './utils';
 
-const store = KeystoneStore.create(storeInitialState as any);
+const store = KeystoneStore.create(storeInitialState);
 
 //@ts-ignore
 window.store = store;
 
-const KeystoneStoreContext = createContext(store);
+const KeystoneStoreContext = createContext<IStore>(store);
 export const MobxStoreProvider = KeystoneStoreContext.Provider;
 
 export const useKeystoneStore = () => {
-  const store = useContext(KeystoneStoreContext);
-  return store
+  return useContext(KeystoneStoreContext);
 };
 
 export const getKeystoneStore = () => {
