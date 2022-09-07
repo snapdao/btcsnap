538ea2ed2243b02407a60000afa1c812b601053f
LiYanLance <liyan1924@gmail.com>
2022-09-07 13:37:40 +0800

    Loading when register and fetch balance

--- packages/example/src/mobx/settings.ts
+++ packages/example/src/mobx/settings.ts
@@ -4,8 +4,6 @@ import { BitcoinNetwork, BitcoinScriptType } from "../interface";
 export const settingsInitialState = {
   network: BitcoinNetwork.Test,
   scriptType: BitcoinScriptType.P2WPKH,
-  currency: 'USD',
-  currencySymbol: '$',
   dynamicAddress: false,
 };
 
@@ -14,19 +12,17 @@ const Settings = types
     network: types.enumeration(Object.values(BitcoinNetwork)),
     scriptType: types.enumeration(Object.values(BitcoinScriptType)),
     dynamicAddress: types.boolean,
-    currency: types.string,
-    currencySymbol: types.string,
   })
   .actions((self) => ({
-    setDynamicAddress:(isDynamic: boolean) => {
-      self.dynamicAddress = isDynamic
-    },
     setNetwork: (network: BitcoinNetwork) => {
       self.network = network;
     },
     setScriptType: (scriptType: BitcoinScriptType) => {
       self.scriptType = scriptType;
-    }
+    },
+    setDynamicAddress:(isDynamic: boolean) => {
+      self.dynamicAddress = isDynamic
+    },
   }))
 
 export default Settings;
