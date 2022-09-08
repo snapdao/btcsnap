45bb9a55fe417a75059f4c83c7930f17296179ff
LiYanLance <liyan1924@gmail.com>
2022-09-08 10:24:53 +0800

    Fetch coins for utxo list in account detail modal

--- packages/example/src/components/Account/Details.tsx
+++ packages/example/src/components/Account/Details.tsx
@@ -1,7 +1,6 @@
 import { Modal } from 'semantic-ui-react';
 import CloseIcon from "../Icons/CloseIcon";
-import {ReactComponent as AccountIcon} from "./image/wallet.svg";
-import AccountList from "./AccountList"
+import { ReactComponent as AccountIcon } from "./image/wallet.svg";
 import {
   AccountDetailTop,
   ModalHeader,
@@ -10,18 +9,71 @@ import {
   AccountDetailHeader,
   AccountTopBalance,
   AccountTopUnits,
-  AccountDetailBottom
+  AccountDetailBottom, AccountListItem, AccountListLabel, AccountListLabelTop, AccountListLabelBottom
 } from "./styles"
+import React, { useEffect, useState } from "react";
+import { sendInfo } from "../../api/v1/sendInfo";
+import { useKeystoneStore } from "../../mobx";
+import { fromHdPathToObj } from "../../lib/cryptoPath";
+import { coinManager } from "../../services/CoinManager";
+import { satoshiToBTC } from "../../lib/helper";
+import { bitcoinUnit } from "./Main";
+import { BitcoinNetwork, Utxo } from "../../interface";
+
+interface CountedUtxo extends Utxo {
+  count: number;
+}
 
 type AccountDetails = {
   open: boolean;
   close: () => void;
-  utxoList: any[];
   balance: number;
   units: string;
 }
 
-const Details = (({open, close, utxoList, balance, units}: AccountDetails) => {
+const Details = (({open, close, balance, units}: AccountDetails) => {
+  const {current} = useKeystoneStore();
+  const [utxoList, setUtxoList] = useState<CountedUtxo[]>([]);
+
+  const switchValue = ((value: number) => {
+    if(units === bitcoinUnit[BitcoinNetwork.Main].BTC || units === bitcoinUnit[BitcoinNetwork.Test].BTC) {
+      return satoshiToBTC(value);
+    } else {
+      return value
+    }
+  })
+
+  useEffect(() => {
+    if (current && open) {
+      sendInfo(current.coinCode).then(data => {
+        const utxoList = data.spendables
+          .map(utxo => {
+            const {change, index} = fromHdPathToObj(utxo.hdPath);
+            const pubkey = coinManager.xpubToPubkey(current.xpub, Number(change || 0), Number(index || 0));
+            return {
+              transactionHash: utxo.txid,
+              index: Number(index || 0),
+              address: coinManager.deriveAddress(pubkey, current.scriptType, current.network),
+              value: utxo.value,
+              path: `M/${change}/${index}`,
+            }
+          }).reduce((acc: CountedUtxo[], cur): CountedUtxo[] => {
+            const itemIndex = acc.findIndex(item => item.path === cur.path);
+            if (itemIndex > -1) {
+              acc[itemIndex].count++;
+              acc[itemIndex].value += cur.value;
+              return acc;
+            } else {
+              return [...acc, {...cur, count: 1}]
+            }
+          }, [])
+        setUtxoList(utxoList)
+      }).catch(e => {
+        console.error("Fetch utxo list failed", e);
+      })
+    }
+  }, [open, current])
+
   return (
     <Modal
       className={'modal-container'}
@@ -33,10 +85,10 @@ const Details = (({open, close, utxoList, balance, units}: AccountDetails) => {
       <AccountDetailTop>
         <ModalHeader>
           <ModalHeaderContainer>
-            <AccountIcon />
+            <AccountIcon/>
             <ModalHeaderLabel>ACCOUNT</ModalHeaderLabel>
           </ModalHeaderContainer>
-          <CloseIcon onClick={close} />
+          <CloseIcon onClick={close}/>
         </ModalHeader>
         <AccountDetailHeader>
           <AccountTopBalance>{balance}</AccountTopBalance>
@@ -44,7 +96,22 @@ const Details = (({open, close, utxoList, balance, units}: AccountDetails) => {
         </AccountDetailHeader>
       </AccountDetailTop>
       <AccountDetailBottom>
-        <AccountList utxoList={utxoList} units={units} />
+        {utxoList.map((item) => (
+          <AccountListItem key={item.address}>
+            <AccountListLabel>
+              <AccountListLabelTop>{item.path}</AccountListLabelTop>
+              <AccountListLabelTop>{switchValue(item.value)}</AccountListLabelTop>
+            </AccountListLabel>
+            <AccountListLabel>
+              <AccountListLabelBottom title={item.address}>
+                {`${item.address.slice(0, 8)}...${item.address.slice(item.address.length - 8)}`}
+              </AccountListLabelBottom>
+              <AccountListLabelBottom>
+                UTXOs:<span>{item.count}</span>
+              </AccountListLabelBottom>
+            </AccountListLabel>
+          </AccountListItem>
+        ))}
       </AccountDetailBottom>
     </Modal>
   )
