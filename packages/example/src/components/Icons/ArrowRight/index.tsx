bd399eed829331bd50d9459cff64b730b16904e9
YanZhe <zhe@keyst.one>
2022-09-08 11:34:42 +0800

    add Setting functions

--- packages/example/src/components/Icons/ArrowRight/index.tsx
+++ packages/example/src/components/Icons/ArrowRight/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import "./index.css"
 
 const ArrowRight = ({size}: {size: number}) => (
-  <svg width={size} height={size} viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M13.3805 9.00054L9.66797 5.28804L10.7285 4.22754L15.5015 9.00054L10.7285 13.7735L9.66797 12.713L13.3805 9.00054Z" fill="#FF6C0A"/>
+  <svg width={size} height={size} viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12.8791 9.00054L9.16663 5.28804L10.2271 4.22754L15.0001 9.00054L10.2271 13.7735L9.16663 12.713L12.8791 9.00054Z" fill="#F58300"/>
   </svg>
 )
 
