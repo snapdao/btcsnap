eec6667c82bda52e969ba95f073c8dc9912a1e2e
LiYanLance <liyan1924@gmail.com>
2022-09-16 10:59:27 +0800

    Move seed check out of Account component

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -3,6 +3,7 @@ import Account from "./components/Account";
 import Connect from "./components/Connect";
 import { getKeystoneStore, MobxStoreProvider } from "./mobx";
 import { trackInit } from "./tracking";
+import { AppCheck } from "./AppCheck";
 
 function App() {
   const mobxStore = getKeystoneStore();
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <MobxStoreProvider value={mobxStore}>
+      <AppCheck/>
       <Connect/>
       <Account/>
     </MobxStoreProvider>
