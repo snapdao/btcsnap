7712e3be4d887456affa0923ee046a2c33fc4972
LiYanLance <liyan1924@gmail.com>
2022-09-16 15:31:53 +0800

    Uplift connect experience

--- packages/example/src/mobx/runtime.ts
+++ packages/example/src/mobx/runtime.ts
@@ -8,16 +8,21 @@ export enum AppStatus {
 
 export const runtimeInitialState = {
   status: AppStatus.Ready,
+  connected: false,
 }
 
 const Runtime = types
   .model('Runtime', {
     status: types.enumeration(Object.values(AppStatus)),
+    connected: types.boolean,
   })
   .actions((self) => ({
     setStatus: (status: AppStatus) => {
       self.status = status
     },
+    setConnected: (hasConnected: boolean) => {
+      self.connected = hasConnected
+    }
   }))
 
 export default Runtime;
