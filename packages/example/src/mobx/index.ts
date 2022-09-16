10b222f513005a7f7558de865e8a8b578a5c921c
LiYanLance <liyan1924@gmail.com>
2022-09-16 15:30:37 +0800

    Persist store in local

--- packages/example/src/mobx/index.ts
+++ packages/example/src/mobx/index.ts
@@ -1,12 +1,20 @@
 import { createContext, useContext } from "react";
 import KeystoneStore, {storeInitialState} from './store';
+import { createPersistor } from './persist';
+import { default as storageAdapter, STORAGE_KEY } from './storage';
 import { IStore } from "./types";
 export * as utils from './utils';
 
 const store = KeystoneStore.create(storeInitialState);
 
-//@ts-ignore
-window.store = store;
+export const {store: persistedStore, persistor} = createPersistor({
+  store,
+  storageKey: STORAGE_KEY,
+  storageAdapter,
+  options: {
+    whitelist: ['accounts', 'current', 'settings', '_version'],
+  },
+});
 
 const KeystoneStoreContext = createContext<IStore>(store);
 export const MobxStoreProvider = KeystoneStoreContext.Provider;
