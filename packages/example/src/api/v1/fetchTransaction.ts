e65d4ebe1cb69e573836ef32201fe3bb86cf0093
aaronisme <aarondongchen@gmail.com>
2022-09-27 10:58:03 +0800

    add the missing file

---
+++ packages/example/src/api/v1/fetchTransaction.ts
@@ -0,0 +1,12 @@
+import { RequestType } from '../types';
+import { query } from "../request-utils/query";
+import { BitcoinNetworkCode } from "../../constant/supportedCoins";
+
+const endpoint = "/v1/self_custody/fetch_raw_tx/";
+
+export const fetchTransaction = (
+  networkCode: BitcoinNetworkCode,
+  txId: string,
+): Promise<Record<string, any>> => {
+  return query(endpoint, RequestType.Get, {}, {coin: networkCode, tx_id: txId });
+};
