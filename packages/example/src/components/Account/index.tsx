b12ea022ac8dfabfaffd29a55ca07e6dc470057d
YanZhe <zhe@keyst.one>
2022-10-09 16:39:10 +0800

    Add privacy pop-up window

--- packages/example/src/components/Account/index.tsx
+++ packages/example/src/components/Account/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { observer } from "mobx-react-lite";
-import { Loader, Modal } from 'semantic-ui-react';
+import { Loader, Modal, Transition } from 'semantic-ui-react';
 import Main from "./Main";
 import Aside from "./Aside";
 import { useBalance } from "../../hook/useBalance";
 import {useKeystoneStore} from "../../mobx";
-import { AccountBackground, AccountContainer, AccountLabel } from "./styles"
+import { AccountBackground, AccountContainer, AccountLabel, CookieInfo, PrivacyLink } from "./styles"
 
 const Account = observer(() => {
-  const { runtime: {isLoading} } = useKeystoneStore();
+  const {persistDataLoaded,  runtime: {isLoading}, user: {isAgreeCookie, agreeCookie} } = useKeystoneStore();
   const { balance, rate, refresh } = useBalance();
 
   return (
@@ -21,6 +21,17 @@ const Account = observer(() => {
           <Main balance={balance} rate={rate} />
           <Aside refreshBalance={refresh}/>
         </AccountContainer>
+        <Transition visible={!isAgreeCookie && persistDataLoaded} animation={'fade up'} duration={'300'}>
+          <CookieInfo>
+            <div>
+              <p>
+                We use cookies to improve the user experience of our product. By continuing to use this site,
+                you agree to our <PrivacyLink>Privacy Policy</PrivacyLink>.
+              </p>
+              <span onClick={agreeCookie}>OK</span>
+            </div>
+          </CookieInfo>
+        </Transition>
         <AccountLabel>Powered by MetaMask Snaps</AccountLabel>
       </AccountBackground>
     </>
