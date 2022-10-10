cdd721272f4119a8bae19e983f8a033d5cd9f6ba
LiYanLance <liyan1924@gmail.com>
2022-10-10 13:09:41 +0800

    Remove clear master fingerprint in wallet

--- packages/example/src/AppCheck.tsx
+++ packages/example/src/AppCheck.tsx
@@ -1,7 +1,6 @@
 import { useKeystoneStore } from "./mobx";
 import { useMFPCheck } from "./hook/useMFPCheck";
 import { useEffect } from "react";
-import { clearMasterFingerprint } from "./lib/snap";
 
 export const AppCheck = () => {
   const { resetStore } = useKeystoneStore();
@@ -11,7 +10,6 @@ export const AppCheck = () => {
     // Clear everything if seed changed in MM
     if(!isSameMFP){
       resetStore();
-      clearMasterFingerprint();
     }
   }, [isSameMFP])
 
