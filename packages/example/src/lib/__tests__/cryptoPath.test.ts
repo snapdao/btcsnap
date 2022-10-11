3664645d69ae4917af5f5cc7358f494d445078fa
YanZhe <zhe@keyst.one>
2022-10-11 14:37:35 +0800

    Bitcoin wallet tests

---
+++ packages/example/src/lib/__tests__/cryptoPath.test.ts
@@ -0,0 +1,15 @@
+import { fromHdPathToObj } from "../cryptoPath";
+
+describe('cryptoPath', () => {
+  it("should convert hdPath to obj", () => {
+    const hdPath = "M/84'/1'/0'/1/8";
+    const params = fromHdPathToObj(hdPath);
+    expect(params).toEqual({account: '0', change: '1', coinType: '1', index: '8', purpose: '84'})
+  })
+
+  it("should return null given wrong hdPath", () => {
+    const hdPath = "abcdefg";
+    const params = fromHdPathToObj(hdPath);
+    expect(params).toEqual({account: null, change: null, coinType: null, index: null, purpose: null})
+  })
+})
