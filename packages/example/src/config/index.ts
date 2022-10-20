1163441c39f959ad0ad596fb49d12d6f226b69c3
leonbit2022 <leonbit2022@gmail.com>
2022-10-20 14:40:30 +0800

    Add sentry dsn into deploy env

--- packages/example/src/config/index.ts
+++ packages/example/src/config/index.ts
@@ -2,7 +2,7 @@ declare var BACKEND_API_DOMAIN: string;
 declare var BACKEND_API_AUTH: string;
 declare var PROJECT_TOKEN: string;
 declare var ENV: "DEVELOPMENT" | "STAGING" | "PRODUCTION";
-declare var SENTRY: string
+declare var SENTRY_SOURCE: string
 
 export const BACKEND_DOMAIN = BACKEND_API_DOMAIN
 export const BACKEND_AUTH = BACKEND_API_AUTH
@@ -11,4 +11,4 @@ export const ENVIRONMENT = ENV || "DEVELOPMENT"
 export const TRACK_TOKEN = PROJECT_TOKEN
 export const VERSION = "1.0.0"
 
-export const SENTRY_DSN = SENTRY
+export const SENTRY_DSN = SENTRY_SOURCE
