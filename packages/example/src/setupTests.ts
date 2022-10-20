cdd1d4631d4f0def8d52890b07780b1953d6bb0d
leonbit2022 <leonbit2022@gmail.com>
2022-10-20 11:06:21 +0800

    Use default test set up file

--- packages/example/src/setupTests.ts
+++ packages/example/src/setupTests.ts
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+jest.mock('./config', () => ({
+  BACKEND_DOMAIN : 'api_host',
+  BACKEND_AUTH : 'api_auth',
+  ENVIRONMENT : "development",
+  TRACK_TOKEN : '',
+  VERSION : "1.0.0",
+}));
