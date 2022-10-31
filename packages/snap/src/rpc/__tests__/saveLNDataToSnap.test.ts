b2043bac3447caa68d8af028e38ee0084bf6e84f
YanZhe <zirancharou16@gmail.com>
2022-10-31 10:57:35 +0800

    Add lightning wallet

---
+++ packages/snap/src/rpc/__tests__/saveLNDataToSnap.test.ts
@@ -0,0 +1,25 @@
+import { WalletMock } from "../__mocks__/wallet";
+import { saveLNDataToSnap } from "../saveLNDataToSnap";
+import { bip44, LNDataToSnap } from "./fixtures/bitcoinNode"
+
+describe('saveLNDataToSnap', () => {
+  const walletStub = new WalletMock();
+
+  afterEach(() => {
+    walletStub.reset()
+  })
+
+  it('should call snap_manageState twice if user save lightning data to Snap', async () => {
+    walletStub.rpcStubs.snap_getBip32Entropy.mockResolvedValue(bip44.slip10Node);
+    await saveLNDataToSnap(
+      LNDataToSnap.domain,
+      walletStub,
+      LNDataToSnap.walletId,
+      LNDataToSnap.credential,
+      LNDataToSnap.password,
+      LNDataToSnap.hdPath
+    );
+    expect(walletStub.rpcStubs.snap_getBip32Entropy).toBeCalledTimes(1);
+    expect(walletStub.rpcStubs.snap_manageState).toBeCalledTimes(3);
+  })
+})
