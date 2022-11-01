8c0be81ed88f60c65dee3916e79fe53fc7d0e7ee
leonbit2022 <leonbit2022@gmail.com>
2022-11-01 16:24:44 +0800

    Add component to add new lightning wallet

---
+++ packages/example/src/components/Icons/AddIcon/styles.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const AddIconContainer = styled.div`
+  box-sizing: border-box;
+  height: 24px;
+  width: 24px;
+  border: 1px solid #E1E6F0;
+  border-radius: 8px;
+  transition: 0.25s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #FFFFFF;
+
+  :hover {
+    background: rgba(0, 0, 0, 0.04);
+    transition: 0.25s;
+    cursor: pointer;
+  }
+`
--- packages/example/src/components/WalletList/WalletList.tsx
+++ packages/example/src/components/WalletList/WalletList.tsx
@@ -1,7 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { TransitionablePortal } from "semantic-ui-react";
+import { ReactComponent as LightningIcon } from './image/lightning.svg';
+import { WalletCard } from "./WalletCard";
+import { useAppStore } from "../../mobx";
+import { AddIcon } from "../Icons/AddIcon";
 import {
-  AccountPageShadow, CurrentPage,
+  AccountPageShadow,
+  CurrentPage,
+  LightningWalletTipsContainer,
+  LightningWalletTipsContent,
+  StyledLightningTipsIcon,
   WalletListContainer,
   WalletListContent,
   WalletListContentContainer,
@@ -10,13 +18,18 @@ import {
 } from "./sytles";
 
 export const WalletList = ({open, close}: any) => {
-  const { current } = useAppStore()
+  const {current} = useAppStore()
   const [visible, setVisible] = useState<boolean>(open)
+  const [isHoveringAddingTips, setIsHoveringAddTips] = useState<boolean>(false)
   const parentNode = useRef<any>()
 
   useEffect(() => {
     setVisible(open)
   }, [open])
+
+  const addLightningWallet = () => {
+    console.log("Add Lightning Wallet");
+  }
 
   return (
     <CurrentPage open={open}>
@@ -36,17 +49,26 @@ export const WalletList = ({open, close}: any) => {
             <WalletListContainer>
               <WalletListHeader>
                 <span>wallets</span>
-                <span>+</span>
+                <AddIcon onClick={addLightningWallet} />
               </WalletListHeader>
 
               <WalletListContentContainer>
                 <WalletListContent>
                   <WalletCard key={current?.id} type={'bitcoin'} balance={0.0001}/>
-                  {
-                    [2, 3].map(wallet => (
-                      <WalletCard key={wallet} type={'lightning'} balance={10000}/>
-                    ))
-                  }
+                  <WalletCard key={'lightning-wallet-01'} type={'lightning'} balance={10000}/>
+                  <LightningWalletTipsContainer
+                    onMouseEnter={() => setIsHoveringAddTips(true) }
+                    onMouseLeave={() => setIsHoveringAddTips(false) }
+                    onClick={addLightningWallet}
+                  >
+                    <StyledLightningTipsIcon>
+                      {isHoveringAddingTips ? <AddIcon /> : <LightningIcon />}
+                    </StyledLightningTipsIcon>
+                    <LightningWalletTipsContent>
+                      <span>Add Lightning Wallets</span>
+                      <p>Fast transaction timings and low transaction fees</p>
+                    </LightningWalletTipsContent>
+                  </LightningWalletTipsContainer>
                 </WalletListContent>
               </WalletListContentContainer>
 
@@ -57,6 +79,3 @@ export const WalletList = ({open, close}: any) => {
     </CurrentPage>
   )
 }
-
-import { WalletCard } from "./WalletCard";
-import { useAppStore } from "../../mobx";
