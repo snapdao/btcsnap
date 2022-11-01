2085ff77a09c2f606c5a288e4d98c1bc6771eea1
leonbit2022 <leonbit2022@gmail.com>
2022-11-01 18:04:35 +0800

    Add lightning state in mobx store

---
+++ packages/example/src/mobx/lightning.ts
@@ -0,0 +1,34 @@
+import {types} from 'mobx-state-tree';
+import LightningWallet from "./lightningWallet";
+
+export const lightningInitialState = {
+  wallets: [],
+  current: undefined,
+}
+
+const Lightning = types
+  .model('Lightning', {
+    wallets: types.array(LightningWallet),
+    current: types.maybe(types.reference(LightningWallet)),
+  })
+  .views((self => ({
+    get hasLightningWallet(){
+      return self.wallets.length > 0
+    },
+    get hasReachedLimitation() {
+      return self.wallets.length >= 10
+    }
+  })))
+  .actions((self => ({
+    createWallet(walletInfo: any): any {
+      const wallet = self.wallets.find((wallet) => wallet.userId === walletInfo.userId);
+      if (wallet) return wallet;
+      return LightningWallet.create(walletInfo);
+    },
+    applyWallet(wallet: any) {
+      self.wallets.push(wallet);
+      self.current = wallet;
+    },
+  })))
+
+export default Lightning;
