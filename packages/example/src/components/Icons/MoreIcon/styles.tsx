be40b8e6b0f0c0dd41d321eb28c8332f50160eb5
leonbit2022 <leonbit2022@gmail.com>
2022-11-01 16:35:40 +0800

    Uplift icons in wallet list

---
+++ packages/example/src/components/Icons/MoreIcon/styles.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const MoreIconContainer = styled.div`
+  box-sizing: border-box;
+  height: 24px;
+  width: 24px;
+  border-radius: 8px;
+  transition: 0.25s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  :hover {
+    background: rgba(0, 0, 0, 0.04);
+    transition: 0.25s;
+    cursor: pointer;
+  }
+`
