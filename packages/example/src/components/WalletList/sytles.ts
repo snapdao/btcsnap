e494600b8d292194cefcd4909a4b26b24db0d533
leonbit2022 <leonbit2022@gmail.com>
2022-11-01 16:24:44 +0800

    Add component to add new lightning wallet

--- packages/example/src/components/WalletList/sytles.ts
+++ packages/example/src/components/WalletList/sytles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { Modal } from "semantic-ui-react";
 import YellowCard from "./image/YellowCard.svg";
 import BlueCard from "./image/BlueCard.svg";
@@ -78,6 +78,7 @@ export const WalletListHeader = styled.div`
 `
 
 export const WalletListContentContainer = styled.div`
+  position: relative;
   width: 100%;
   height: calc(624px);
   overflow-y: scroll;
@@ -126,4 +127,47 @@ export const WalletCardAmount = styled.div`
   font-size: 20px;
   line-height: 28px;
   color: #F7F9FC;
+`
+
+export const LightningWalletTipsContainer = styled.div`
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  height: 92px;
+  width: 240px;
+  border: 1px solid #E1E6F0;
+  border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  transition: 0.25s;
+  background-color: #FFFFFF;
+
+  :hover {
+    background: rgba(0, 0, 0, 0.04);
+    transition: 0.25s;
+    cursor: pointer;
+  }
+`
+
+export const LightningWalletTipsContent = styled.div`
+  padding: 16px 16px 16px 48px;
+  span {
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 20px;
+    color: #111214;
+  }
+  p {
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 18px;
+    color: #9095A3;
+  }
+`
+
+export const StyledLightningTipsIcon = styled.div`
+  position: absolute;
+  top: 16px;
+  left: 16px;
 `
