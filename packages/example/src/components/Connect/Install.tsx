5b9d5b8e492bafa4f36f36fae39668b5ba04c998
leonbit2022 <leonbit2022@gmail.com>
2022-11-02 15:35:34 +0800

    Show different Flask download link for Firefox

--- packages/example/src/components/Connect/Install.tsx
+++ packages/example/src/components/Connect/Install.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { ReactComponent as FlaskIcon } from "./image/MetaMaskFlask.svg"
 import { ReactComponent as InstallIcon } from "./image/install.svg"
 import Modal from "./Modal";
+import { isFirefox } from "../../lib/helper";
 
 interface InstallProps {
   open: boolean;
   close: () => void;
   isFirstStep?: boolean;
+}
+
+const getFlaskInstallLink = () => {
+  return isFirefox(navigator.userAgent)
+    ? 'https://addons.mozilla.org/en-US/firefox/addon/metamask-flask/'
+    : 'https://chrome.google.com/webstore/detail/metamask-flask-developmen/ljfoeinjpaedjfecbmggjgodbgkmjkjk'
 }
 
 const Install = ({open, close, isFirstStep}: InstallProps) => {
@@ -17,7 +24,7 @@ const Install = ({open, close, isFirstStep}: InstallProps) => {
       <p className="Connect-install">You will need to install the MetaMask Flask extension in order to use Bitcoin Snap.</p>
       <p className="Connect-flask">Please disable the regular MetaMask extension prior to using flask.</p>
       <a
-        href="https://chrome.google.com/webstore/detail/metamask-flask-developmen/ljfoeinjpaedjfecbmggjgodbgkmjkjk"
+        href={getFlaskInstallLink()}
         className="Connect-button"
         target="_blank"
         rel="noopener noreferrer"
