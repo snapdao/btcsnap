84b6aec4dc5d1abfcef0f2d9d75b732cdc499889
YanZhe <zirancharou16@gmail.com>
2022-11-03 16:44:07 +0800

    Add user guide for Bitcoin button

---
+++ packages/example/src/components/SetupLightning/Tooltip.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  Container,
+  Text,
+  Button
+} from "./styles"
+
+
+interface iPageTourModal {
+  continuous: boolean // If the tour is continuous or not
+  index: number // The current step's index
+  isLastStep: boolean // The name says it all
+  size: number // The number of steps in the tour
+  step: any // The current step data
+  backProps: any // The back button's props
+  closeProps: any // The close button's props
+  primaryProps: any // The primary button's props (Close or Next if the tour is continuous)
+  skipProps: any // The skip button's props
+  tooltipProps: any // The root element props (including ref)
+}
+
+// Component of User guide component
+
+const Tooltip = ({
+  continuous,
+  index,
+  isLastStep,
+  size,
+  step,
+  backProps,
+  closeProps,
+  primaryProps,
+  skipProps,
+  tooltipProps,
+}: iPageTourModal) => {
+
+  return (
+    <Container {...tooltipProps}>
+      <Text>You can always create lightning wallets here any time later.</Text>
+      <Button {...closeProps}>Got It</Button>
+    </Container>
+  );
+};
+
+export default Tooltip;
