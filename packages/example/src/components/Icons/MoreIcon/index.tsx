ab828beef9e7f282ddcde006633b5f782dc7cf93
leonbit2022 <leonbit2022@gmail.com>
2022-11-04 14:10:28 +0800

    Refactor Wallet List

--- packages/example/src/components/Icons/MoreIcon/index.tsx
+++ packages/example/src/components/Icons/MoreIcon/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { MoreIconContainer } from "./styles";
 
-export const MoreIcon = (props: React.ButtonHTMLAttributes<HTMLDivElement>) => {
+export const MoreIcon = (props: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
     <MoreIconContainer {...props}>
       <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
--- packages/example/src/components/WalletList/WalletCard/index.tsx
+++ packages/example/src/components/WalletList/WalletCard/index.tsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import { BitcoinUnit } from "../../../interface";
+import React, { useCallback } from 'react'
+import { BitcoinUnit, WalletType } from "../../../interface";
 import { WalletCardBalance, WalletCardContainer, WalletCardContent, WalletCardHeader, } from "./styles";
 import { MoreIcon } from "../../Icons/MoreIcon";
+import { useAppStore } from "../../../mobx";
+import { observer } from "mobx-react-lite";
 
 interface WalletCardProps extends React.ButtonHTMLAttributes<HTMLDivElement> {
-  walletType: 'bitcoin' | 'lightning';
-  id?: string;
-  name?: string;
+  id: string;
+  walletType: WalletType;
+  name: string;
   balance: string | number;
   unit: BitcoinUnit;
   selected: boolean
   onClick?: () => void
+  available?: boolean
 }
 
-export const WalletCard = ({walletType, balance, selected, onClick, name, unit}: WalletCardProps) => {
+export const WalletCard = observer(({id, walletType, balance, selected, onClick, name, unit, available = true}: WalletCardProps) => {
+  const { lightning } = useAppStore()
+
+  const edit = useCallback((event) => {
+    event.stopPropagation();
+    if(walletType === WalletType.LightningWallet) {
+      // TODO: render edit modal
+      lightning.removeWallet(id);
+    }
+  }, [id, walletType])
+
   return (
     <WalletCardContainer active={selected} onClick={onClick}>
-      <WalletCardContent type={walletType}>
+      <WalletCardContent type={walletType} available={available}>
         <WalletCardHeader>
-          <span>{name || walletType}</span>
-          <MoreIcon/>
+          <span>{name}</span>
+          <MoreIcon onClick={edit} disabled={!available}/>
         </WalletCardHeader>
         <WalletCardBalance>
           {balance} {unit}
@@ -27,4 +40,4 @@ export const WalletCard = ({walletType, balance, selected, onClick, name, unit}:
       </WalletCardContent>
     </WalletCardContainer>
   )
-}
+})
