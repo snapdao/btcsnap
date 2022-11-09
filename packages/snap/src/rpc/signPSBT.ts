87be11b511260ed3a84762ced3baa052d9da1fe9
leonbit2022 <leonbit2022@gmail.com>
2022-11-09 15:37:56 +0800

    Replace errors with SnapError

--- packages/snap/src/rpc/signPSBT.ts
+++ packages/snap/src/rpc/signPSBT.ts
@@ -3,11 +3,12 @@ import { extractAccountPrivateKey } from './getExtendedPublicKey';
 import { AccountSigner, BtcTx } from '../bitcoin';
 import { getPersistedData } from '../utils/manageState';
 import { getNetwork } from '../bitcoin/getNetwork';
+import { SnapError, RequestErrors } from "../errors";
 
 export async function signPsbt(domain: string, wallet: Wallet, psbt: string, network: BitcoinNetwork, scriptType: ScriptType): Promise<{ txId: string, txHex: string }> {
   const snapNetwork = await getPersistedData<BitcoinNetwork>(wallet, "network", '' as BitcoinNetwork);
   if(!snapNetwork){
-    throw new Error('Network not match')
+    throw SnapError.of(RequestErrors.NetworkNotMatch);
   }
 
   const btcTx = new BtcTx(psbt, snapNetwork);
@@ -28,6 +29,6 @@ export async function signPsbt(domain: string, wallet: Wallet, psbt: string, net
     btcTx.validateTx(signer)
     return btcTx.signTx(signer)
   } else {
-    throw new Error('User reject the sign request')
+    throw SnapError.of(RequestErrors.RejectSign);
   }
 }
