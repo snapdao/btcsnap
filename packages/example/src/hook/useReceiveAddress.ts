66b6cc64fb544ff226fb2ad77d259a478ea87eb0
leonbit2022 <leonbit2022@gmail.com>
2022-11-10 10:09:32 +0800

    Refactor receiveAddress generate logic

--- packages/example/src/hook/useReceiveAddress.ts
+++ packages/example/src/hook/useReceiveAddress.ts
@@ -25,23 +25,16 @@ export const useReceiveAddress = () => {
     } catch (e) {
       logger.error(e);
       return {
-        index: current.receiveAddressIndex,
-        address: current.getReceiveAddress()
+        index: current.receiveAddress.index,
+        address: current.receiveAddress.address
       }
     }
   }, [])
 
   const setReceiveAddress = (current: IAccount) => {
-    const receiveAddress = current.getReceiveAddress();
-    if(receiveAddress) {
-      setAddress(receiveAddress.address);
-      setPath(`M/0/${receiveAddress.index}`);
-    } else {
-      const address = current.deriveAddress(0);
-      current.addAddress(address, dynamicAddress);
-      setAddress(address.address);
-      setPath(`M/0/${address.index}`);
-    }
+    const receiveAddress = current.receiveAddress;
+    setAddress(receiveAddress.address);
+    setPath(`M/0/${receiveAddress.index}`);
   }
 
   useEffect(() => {
