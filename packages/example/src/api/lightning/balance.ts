dc2872c3d7609a854d986132fc6f75689d8a98d2
xuan <xuanhiwork@gmail.com>
2022-11-11 11:53:13 +0800

    fix: lightning api

--- packages/example/src/api/lightning/balance.ts
+++ packages/example/src/api/lightning/balance.ts
@@ -3,19 +3,7 @@ import { RequestType } from '../types';
 
 const endpoint = `/v1/snap/balance/`;
 
-export type CreateWalletResponse = {
-  userId: string;
-  userPassword: string;
-  credential: {
-    login: string;
-    password: string;
-  };
-};
-
-export const balance = (
-  userId: string,
-  userPassword: string,
-): Promise<CreateWalletResponse> => {
+export const balance = (userId: string, userPassword: string) => {
   return query(
     endpoint,
     RequestType.Post,
