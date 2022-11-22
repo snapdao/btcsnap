6be8595b6dff73e9e5f3586e4f27edbe55bb4edc
leonbit2022 <leonbit2022@gmail.com>
2022-11-22 11:47:02 +0800

    Add Record Card component

---
+++ packages/example/src/components/TransactionList/RecordCard/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { HistoryRecord } from "../../../types";
+import { StyledRecordCard } from "./styles";
+import { recordToRecordCard } from "./transfrom";
+
+interface RecordCardProps {
+  record: HistoryRecord;
+  onClick?: () => void;
+}
+
+export const RecordCard = ({record, onClick}: RecordCardProps) => {
+  return (
+    <StyledRecordCard
+      {...recordToRecordCard(record)}
+      onClick={onClick}
+    />
+  )
+}
