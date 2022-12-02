7545d95304c0bb4f879ad2071aa8bdb0a4d0b228
leonbit2022 <leonbit2022@gmail.com>
2022-12-02 10:04:48 +0800

    Add snap method for get all xpubs

--- packages/snap/src/rpc/getMasterFingerprint.ts
+++ packages/snap/src/rpc/getMasterFingerprint.ts
@@ -3,12 +3,12 @@ import { CRYPTO_CURVE, pathMap } from '../rpc/getExtendedPublicKey';
 
 export async function getMasterFingerprint(wallet: Wallet): Promise<string | void> {
   const slip10Node = await wallet.request({
-    method: "snap_getBip32Entropy",
+    method: 'snap_getBip32Entropy',
     params: {
       path: pathMap.P2WPKH,
       curve: CRYPTO_CURVE
     },
-  }) as SLIP10Node
+  }) as SLIP10Node;
 
-  return slip10Node.masterFingerprint && slip10Node.masterFingerprint.toString(16)
+  return slip10Node.masterFingerprint && slip10Node.masterFingerprint.toString(16);
 }
