b5baef2db9d40ad3741e21ede73ae446611a399b
leonbit2022 <leonbit2022@gmail.com>
2023-02-23 14:33:32 +0800

    Bump btcsnap version to 1.3.0

--- packages/example/src/lib/snap.ts
+++ packages/example/src/lib/snap.ts
@@ -20,7 +20,7 @@ export async function connect(cb: (connected: boolean) => void) {
       method: 'wallet_requestSnaps',
       params: {
         [snapId]: {
-          version: '1.2.0',
+          version: '1.3.0',
         },
       }
     });
