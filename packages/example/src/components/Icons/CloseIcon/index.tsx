317b268e11282047cd853a50302cde4035a1394c
leonbit2022 <leonbit2022@gmail.com>
2023-02-27 16:20:20 +0800

    Show important notice in account page for every wallet

--- packages/example/src/components/Icons/CloseIcon/index.tsx
+++ packages/example/src/components/Icons/CloseIcon/index.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './index.css';
 
 interface CloseIconProps {
+  size?: 'small' | 'medium';
   onClick: () => void,
   isDisabled?: boolean
 }
 
-const CloseIcon = ({ onClick, isDisabled }: CloseIconProps) => {
+const CloseIcon = ({ onClick, isDisabled, size = 'medium' }: CloseIconProps) => {
+  const svgSize = size === 'medium' ? 14 : 9;
+  const disabledClass = isDisabled ? 'Close-container-disabled' : 'Close-container';
+  const sizeClass = size === 'medium' ? '' : 'small';
   return (
-    <div className={isDisabled ? 'Close-container-disabled' : 'Close-container'} onClick={onClick}>
-      <svg width='14' height='14' viewBox='0 0 14 14' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    <div className={`${disabledClass} ${sizeClass}`} onClick={onClick}>
+      <svg width={svgSize} height={svgSize} viewBox='0 0 14 14' fill='none' xmlns='http://www.w3.org/2000/svg'>
         <path d='M1 1L13 13M13 1L1 13' stroke='#111214' strokeWidth='1.5'/>
       </svg>
     </div>
