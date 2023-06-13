bd2a5ef10f91e1a1d6bfd50d3fa85c678f1ee972
Charon-Fan <Charon.Dian@gmail.com>
2023-06-13 15:14:06 +0800

    refactor: remove unused files

--- packages/snap-mainnet/snap/src/errors/constant/InvoiceErrors.ts
+++
@@ -1,6 +0,0 @@
-export const InvoiceErrors = {
-  AmountNotValid: {
-    code: 30001,
-    message: 'Amount is not valid',
-  },
-};
--- packages/snap-mainnet/snap/src/errors/index.ts
+++
@@ -1,4 +0,0 @@
-export { SnapError } from './SnapError';
-export { PsbtValidateErrors } from './constant/PsbtValidaeErrors';
-export { RequestErrors } from './constant/RequestErrors';
-export { InvoiceErrors } from './constant/InvoiceErrors';
