250f89b85c6e5b07cc7e7f54ae76394e2645f763
Charon-Fan <Charon.Dian@gmail.com>
2023-06-16 14:51:26 +0800

    refactor: optimize network helper

--- packages/snap/src/utils/network.ts
+++ packages/snap/src/utils/network.ts
@@ -16,33 +16,26 @@ export const getNetwork = (snapType: BitcoinNetwork) => {
   return determineNetworkType(snapType) === BitcoinNetwork.Main ? networks.bitcoin : networks.testnet;
 }
 
+export const getNetworkFromSnapType = getNetwork;
+
 export const getCoinType = (snapType?: BitcoinNetwork) => {
   return determineNetworkType(snapType) === BitcoinNetwork.Main ? BITCOIN_MAINNET_COIN_TYPE : BITCOIN_TESTNET_COIN_TYPE;
 }
+
+export const getCoinTypeFromSnapType = getCoinType;
 
 export const getAddressPattern = (snapType?: BitcoinNetwork) => {
   return determineNetworkType(snapType) === BitcoinNetwork.Main ? BITCOIN_MAIN_NET_ADDRESS_PATTERN : BITCOIN_TEST_NET_ADDRESS_PATTERN;
 }
 
-// XXX: Keep redundant functions for future adjustments.
+export const getAddressPatternFromSnapType = getAddressPattern;
+
 export const getNetworkFromCoinType = (coinType?: number | string): Network => {
   return getNetwork([0, '0'].includes(coinType) ? BitcoinNetwork.Main : BitcoinNetwork.Test);
 }
 
 export const getSnapTypeFromNetwork = (network?: Network): BitcoinNetwork => {
   return determineNetworkType(network == networks.bitcoin ? BitcoinNetwork.Main : BitcoinNetwork.Test);
-}
-
-export const getNetworkFromSnapType = (snapType?: BitcoinNetwork): Network => {
-  return getNetwork(snapType);
-}
-
-export const getCoinTypeFromSnapType = (snapType?: BitcoinNetwork) => {
-  return getCoinType(snapType);
-}
-
-export const getAddressPatternFromSnapType = (snapType?: BitcoinNetwork) => {
-  return getAddressPattern(snapType);
 }
 
 export const getAllTheSupportedSnapTypes = () => {
